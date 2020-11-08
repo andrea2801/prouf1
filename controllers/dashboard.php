f1a8132413175df11d7d485080e6fa1805c26706
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2020-11-08 21:28:10 +0100

    Update dashboard.php

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -1,7 +1,31 @@
 <?php
+include "shema.php";
+include 'config.php';
+include 'src/connect.php';
 //render vista
 require APP.'/src/render.php';
 //si esta definida la session
 $user=$_SESSION['username'] ?? ''; /*??=operador cualescencia si no esta 
 definido entonces email seria ''*/
+$des=filter_input(INPUT_POST, "des", FILTER_SANITIZE_SPECIAL_CHARS);
+$userid= filter_input(INPUT_POST, "user", FILTER_SANITIZE_SPECIAL_CHARS);
+$date= filter_input(INPUT_POST, "date", FILTER_SANITIZE_SPECIAL_CHARS);
+$tarea= filter_input(INPUT_POST, "tarea", FILTER_SANITIZE_SPECIAL_CHARS);
 echo render('dashboard',['Usuario'=>'Hola '.$user]);
+$db=connectMysql($dsn,$dbuser,$dbpass);
+$table="Task";
+
+
+if(isset($userid) && isset($des) && isset($date)){
+
+    $date = ['user' => $userid,'description' => $des,'due_date' => $date];
+    insertTareas($db,$table,$date);
+   
+  
+}
+if($tarea!=null){
+
+    deleteTarea( $db, $tarea);
+   
+  
+}
