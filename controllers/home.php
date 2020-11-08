26bb6f9e8e971cde266875e36f97d3b66f2d0c2d
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2020-11-08 20:39:11 +0100

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -1,7 +1,17 @@
 <?php
+include "shema.php";
+include 'config.php';
+include 'src/connect.php';
 //render vista
 require APP.'/src/render.php';
 //si esta definida la session
 $user=$_SESSION['username'] ?? ''; /*??=operador cualescencia si no esta 
 definido entonces email seria ''*/
-echo render('home',['Usuario'=>'Bienvenid@ '.$user.'.']);
+$db=connectMysql($dsn,$dbuser,$dbpass);
+$table="Task";
+$fields=['id','description','user','due_date'];
+$selectAll=selectAll($db,$table,$fields);
+echo render('home',['Usuario'=>'Bienvenid@ '.$user, 'Tarea' => $selectAll]);
+
+
+
