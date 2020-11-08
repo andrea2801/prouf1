f26ba7849931d7360e18dfb1188059eb240536c9
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2020-11-08 20:36:54 +0100

    Update start.php

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 //crea el esquema
 include 'config.php';
 require 'src/connect.php';
-$db=connectMysql($dsn,$dbuser,$dbpass);
+//$db=connectMysql($dsn,$dbuser,$dbpass);
 $sql=file_get_contents('prouf1.sql');
 try{
 	$db->exec($sql);
