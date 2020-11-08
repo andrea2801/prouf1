efcc835ac0031a5bec34c08bb3f05d2871f6c764
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2020-11-08 18:51:51 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost="127.0.0.1";
-$dbname="prouf1";
-$dbuser="prouf1";
+$dbname="AlAn_prouf1";
+$dbuser="AlAn_prouf1";
 $dbpass="linuxlinux";
 $dsn='mysql:host='.$dbhost.';dbname='.$dbname.';';
