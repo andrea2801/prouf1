e8d69d13b57ea680c112e28ff158ccd2f54fbf5e
andrea2801 <71711841+andrea2801@users.noreply.github.com>
2020-11-08 20:35:23 +0100

    Update home.tpl.php

--- src/templates/home.tpl.php
+++ src/templates/home.tpl.php
@@ -3,19 +3,31 @@
 include 'src/templates/header.tpl.php';
 ?>
 <main id="home"><h1><?=$Usuario;?></h1>
-<p class="homes">Empieza a gestionar tus tareas<p><a href="?url=dasboard">  Editar</a>
-     <?php
-     include 'config.php';
-     require 'src/connect.php';
-     $db=connectMysql($dsn,$dbuser,$dbpass);
-     $name=$_SESSION['username'];
-     if ($db) {
-                        $querySelect = "SELECT username FROM Users WHERE username='$name'";
-                        $taskuser = [];
-                        $db->exec($querySelect, $taskuser);
-                        taskUser($taskuser);
-                    }
-                    ?>
+<p class="homes">Empieza a gestionar tus tareas<p><a href="?url=dasboard">Editar</a>
+<?php
+        if(isset($data['Tarea'])){
+        if(count($data['Tarea']) > 0){
+    ?>  
+<table class="table">
+            <thead>
+            <tr>
+                <th>Task ID</th>
+                <th>Description</th>
+                <th>User ID</th>
+                <th>Date</th>
+            </tr>
+            </thead>
+            <?php
+
+foreach($data['Tarea'] as $valor){
+    echo "<tr>";
+        foreach($valor as $fields){
+            echo "<td>".$fields."</td>";
+        }
+            echo "</tr>";
+}}}
+?>
+</table>
 </main>
 <?php
 include 'src/templates/footer.tpl.php';
